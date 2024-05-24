c214ecff1de6ebafd6e7ec642c1c042f3df35a99
陽斗 森 <junhiro3284@gmail.com>
2024-05-24 09:35:08 +0900

    無改造

---
+++ SourceCode/all.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDED_ALL
+#define INCLUDED_ALL
+
+//******************************************************************************
+//
+//
+//      all.h
+//
+//
+//******************************************************************************
+
+//------< インクルード >---------------------------------------------------------
+#include "../GameLib/game_lib.h"
+#include "../GameLib/input_manager.h"
+#include "../GameLib/obj2d_data.h"
+#include "../GameLib/depth_stencil.h"
+
+#include "work.h"
+#include "user.h"
+#include "common.h"
+#include "obj2d.h"
+#include "sprite_data.h"
+
+#include "player.h"
+
+#include "scene.h"
+#include "game.h"
+#include "title.h"
+
+//******************************************************************************
+
+#endif // !INCLUDED_ALL
---
+++ SourceCode/game.cpp
@@ -0,0 +1,143 @@
+//******************************************************************************
+//
+//
+//      ゲーム
+//
+//
+//******************************************************************************
+
+//------< インクルード >---------------------------------------------------------
+#include "all.h"
+
+//------< using >---------------------------------------------------------------
+using namespace GameLib;
+
+//------< 変数 >----------------------------------------------------------------
+Game Game::instance_;
+
+//--------------------------------
+//  初期化処理
+//--------------------------------
+void Game::init()
+{
+    Scene::init();	    // 基底クラスのinitを呼ぶ
+
+    playerManager_      = new PlayerManager;
+
+    isPaused = false;   // ポーズフラグの初期化
+}
+
+//--------------------------------
+//  終了処理
+//--------------------------------
+void Game::deinit()
+{
+    // 各マネージャの解放
+    safe_delete(playerManager_);
+
+    // テクスチャの解放
+    texture::releaseAll();
+
+    // 音楽のクリア
+    music::clear();
+}
+
+//--------------------------------
+//  更新処理
+//--------------------------------
+void Game::update()
+{
+    using namespace input;
+
+    // ソフトリセット
+    if ((STATE(0) & PAD_SELECT) &&  // 0コンのセレクトボタンが押されている状態で
+        (TRG(0) & PAD_START))       // 0コンのスタートボタンが押された瞬間
+    {
+        changeScene(Title::instance());   // タイトルシーンに切り替える
+        return;
+    }
+
+    // デバッグ文字列表示
+    debug::setString("state:%d", state);
+    debug::setString("timer:%d", timer);
+
+    // ポーズ処理
+    if (TRG(0) & PAD_START)
+        isPaused = !isPaused;       // 0コンのスタートボタンが押されたらポーズ状態が反転
+    if (isPaused) return;           // この時点でポーズ中ならリターン
+
+    switch (state)
+    {
+    case 0:
+        //////// 初期設定 ////////
+
+        timer = 0;
+        GameLib::setBlendMode(Blender::BS_ALPHA);   // 通常のアルファ処理
+
+        // テクスチャの読み込み
+        texture::load(loadTexture);
+
+        // プレイヤーマネージャの初期化
+        playerManager()->init();
+
+        // プレイヤー（自分で操作）を追加する
+        playerManager()->add(&player, VECTOR2(window::getWidth() / 2, window::getHeight() / 2));
+
+        state++;    // 初期化処理の終了
+
+        /*fallthrough*/     // case 1:の処理も同時に行う必要があるため、わざとbreak;を記述していない
+
+    case 1:
+        //////// 通常時の処理 ////////
+
+        timer++;
+
+        // プレイヤーの更新
+        playerManager()->update();
+
+        break;
+    }
+}
+
+//--------------------------------
+//  描画処理
+//--------------------------------
+void Game::draw()
+{
+    // 画面クリア
+    GameLib::clear(VECTOR4(0, 0, 0, 1));
+
+#if 2
+//******************************************************************************
+// TODO:02 地面の描画
+//------------------------------------------------------------------------------
+/*
+課題）
+    地面を描画しなさい
+
+ヒント）
+    0, GROUND_POS_Yから、幅 window::getWidth()、高さ window::getHeight() - GROUND_POS_Yの
+    矩形を描画しなさい。色は背景色以外であれば、何色でも良い。
+*/
+//******************************************************************************
+#endif
+    //TODO_02 地面の描画
+    primitive::rect(0, GROUND_POS_Y,
+        window::getWidth(), window::getHeight()-GROUND_POS_Y,0,0,ToRadian(0)
+    ,1,0,0);
+
+    for (int i = 0; i < TERRAIN_NUM; ++i)
+    {
+        primitive::rect(
+            terrai[i].pos,
+            terrai[i].hsize * 2,
+            terrai[i].hsize, 0,
+            { 1, 1, 1, 1 }
+        );
+    }
+
+    // プレイヤーの描画
+    playerManager()->draw();
+}
+
+//******************************************************************************
