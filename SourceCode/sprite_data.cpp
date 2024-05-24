f6a305bd3e77e37468e2d5abad501e5795fcd566
yzryh <167487698+yzryh@users.noreply.github.com>
2024-05-24 12:03:36 +0900

    a

--- SourceCode/sprite_data.cpp
+++ SourceCode/sprite_data.cpp
@@ -17,8 +17,9 @@ using namespace GameLib;
 // 2D画像ロードデータ
 LoadTexture loadTexture[] = {
    { TEXNO::PLAYER,     L"./Data/Images/player.png",    1U },// プレイヤー
-    { TEXNO::BACK_WHITE, L"./Data/Images/White.png",     1U },//白い背景
-    { -1, nullptr }	// 終了フラグ
+   { TEXNO::BACK_WHITE, L"./Data/Images/White.png",     1U },//白い背景
+   { TEXNO::BACK_BLACK, L"./Data/Images/Black.png",      1U },//黒い背景
+   { -1, nullptr }	// 終了フラグ
 };
 
 //------< マクロ >--------------------------------------------------------------
